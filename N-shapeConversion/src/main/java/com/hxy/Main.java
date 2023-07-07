5612121f76c98b0c0d4e0c4c41e6629ecdb310a9
Young <h_xyoung@163.com>
2023-07-07 12:09:01 +0800

    N自行转换

--- N-shapeConversion/src/main/java/com/hxy/Main.java
+++ N-shapeConversion/src/main/java/com/hxy/Main.java
@@ -1,21 +1,52 @@
 package com.hxy;
 
-import java.util.Arrays;
-import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
+        String s = "Apalindromeisaword,phrase,number,orothersequenceofunitsthatcanbereadthesamewayineitherdirection,withgeneralallowancesforadjustmentstopunctuationandworddividers.";
+        String convert = convert(s, 4);
+        System.out.println(convert);
 
     }
 
-    public String convert(String s, int n) {
+    public static String convert(String s, int n) {
         int length = s.length();
-        int column;
-        int remainder = length % (2 * n - 2) == 0；
+        if (n == 1 || n >= length) {
+            return s;
+        }
+        int column = getColumnNum(length, n);
+        char[][] resultArr = new char[n][column];
+        int x = 0, y = 0;
+        for (int i = 0 ; i < length; i++) {
+            resultArr[y][x] = s.charAt(i);
+            if (i % (2 * n - 2) < n - 1) {
+                y++;
+            }else {
+                x++;
+                y--;
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (char[] temp: resultArr){
+            for (char temp2 : temp){
+                if (temp2 != 0) {
+                    result.append(temp2);
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    public static int getColumnNum(int length, int n){
+        int column = 1;
+        int remainder = length % (2 * n - 2);
         if (remainder == 0){
             column = (length / (2 * n - 2)) * (n -1);
-        }else {
-            column = (length / (2 * n - 2)) + 1 ;
+        }else if (remainder <= n){
+            column = (length / (2 * n - 2)) * (n -1) + 1 ;
+        }else if (remainder > n){
+            column = (length / (2 * n - 2)) * (n -1) + 1 + remainder - n;
         }
+        return column;
     }
 }
